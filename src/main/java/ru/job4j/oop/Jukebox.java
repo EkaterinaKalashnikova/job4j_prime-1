191120e63ae4e57dccdfd22e4ee8773bc53c3744
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-10-22 20:51:23 +0300

    Вызов метода с аргументами. [#166086]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -1,17 +1,22 @@
 package ru.job4j.oop;
 
 public class Jukebox {
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        } else if (position == 2) {
+            System.out.println("Спокойной ночи");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
 
+    public static void main(String[] args) {
+        Jukebox jukebox = new Jukebox();
+        jukebox.music(1);
+        jukebox.music(2);
+        jukebox.music(3);
+        System.out.println(jukebox);
+    }
 }
 
-   /** Добавьте в него метод public void music(int position).
-
-        3. Внутри метода сделайте if else.
-
-        Если position равно 1, то в консоль должны выводиться слова песни "Пусть бегут неуклюже".
-
-        Если равно 2, то в консоль должны выводиться слова песни "Спокойной ночи".
-
-        Во всех остальных случаях должно появиться "Песня не найдена".
-
-        4. Напишите метод main с демонстрацией работы этого метода и объекта этого класса. */
