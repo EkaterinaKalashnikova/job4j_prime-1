aa8dae735160a630ab796e460a7bdc2cb57fe0c3
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-10-25 08:21:29 +0300

    Состояние объекта [#167388 #219659]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,15 +8,15 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load - another.load;
-        another.load += another.load;
+        this.load = this.load + another.load;
+        another.load = 0;
     }
 
     public static void main(String[] args) {
-        Battery first = new Battery(100);
-        Battery second = new Battery(15);
-        Battery thrid = new Battery(75);
-        Battery fourth = new Battery(30);
+        Battery first = new Battery(15);
+        Battery second = new Battery(30);
+        Battery thrid = new Battery(25);
+        Battery fourth = new Battery(20);
         System.out.println("first : " + first.load + ". second : " + second.load);
         first.exchange(second);
         System.out.println("first : " + first.load + ". second : " + second.load);
@@ -26,4 +26,3 @@ public class Battery {
         System.out.println("thrid : " + thrid.load + ". fourth : " + fourth.load);
     }
 }
-
