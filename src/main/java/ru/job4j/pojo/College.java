c185f080868e1fb7725942955663048b9b0fc9c0
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-10-27 18:14:09 +0300

    Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,23 @@
+package ru.job4j.pojo;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Ekaterina");
+        student.setSurname("Kalashnikova");
+        student.setGroup(1);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMM dd HH:mm:ss");
+        Calendar data = new GregorianCalendar(2018,Calendar.AUGUST,25,13,24,56);
+        data.set(2018, Calendar.AUGUST, 25);
+        student.setStart(sdf.format(data.getTime()));
+        System.out.println("Имя студента: " + student.getName());
+        System.out.println("Фамилия студента: " + student.getSurname());
+        System.out.println("Номер группы: " + student.getGroup());
+        System.out.println("Дата поступления: " + student.getStart());
+    }
+}
+
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,44 @@
+package ru.job4j.pojo;
+
+public class Student {
+   private String name;
+   private String surname;
+   private int group;
+   private String start;
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getSurname() {
+      return surname;
+   }
+
+   public void setSurname(String surname) {
+      this.surname = surname;
+   }
+
+   public int getGroup() {
+      return group;
+   }
+
+   public void setGroup(int group) {
+      this.group = group;
+   }
+
+   public String getStart() {
+      return start;
+   }
+
+   public void setStart(String start) {
+      this.start = start;
+   }
+}
+
+
+
+
