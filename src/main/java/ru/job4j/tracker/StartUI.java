05bc5242a2bf7cb7619a4864bc7e74a014665de0
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-11-07 20:27:33 +0300

    toString [#310018 #225156]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,10 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        LocalDateTime created = item.getCreated();
-        System.out.println(created);
+        item.setId(2);
+        item.setName("dog");
+        System.out.println(item);
     }
 }
