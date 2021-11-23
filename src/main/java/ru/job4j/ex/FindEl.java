ed4e33f535ace43bd40caa16b05f5c70218185f5
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-11-23 19:51:29 +0300

    Пользовательские исключения. [#219367 #230912]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -15,7 +15,7 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] != null && value[i].equals(key) || key == null && value[i] == null) {
+            if (value[i].equals(key)) {
                 return i;
             }
         }
