f5792ffca0e5c3b38d5f77ae641fdb7cbd528ad6
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-11-23 19:47:53 +0300

    Тестирование исключений с junit [#219366 #230897]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -7,12 +7,12 @@ import static org.hamcrest.core.Is.is;
 
 public class FactTest {
 
-    /** @Test(expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void whenStartFactorialCalc() {
         Fact fact = new Fact();
-        int rsl = fact.calc(5);
+        int rsl = fact.calc(-5);
         Assert.assertThat(rsl, is(120));
-    }*/
+    }
 
     @Test
     public void whenStartFactCalc() {
