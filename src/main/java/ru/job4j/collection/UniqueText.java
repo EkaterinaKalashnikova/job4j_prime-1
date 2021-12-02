ff10ad67c00e35456a088a3cab90579828cda807
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-12-02 18:53:54 +0300

    Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String str : origin) {
+            check.add(str);
+        }
+        for (String s : text) {
+            if (check.contains(s)) {
+                return rsl;
+            }
+            rsl = false;
+        }
+        return rsl;
+    }
+}
+
