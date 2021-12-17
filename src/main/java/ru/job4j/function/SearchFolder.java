0a45332d99867e951c24b721868d140d30c41ae6
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-12-17 14:18:58 +0300

    Встроенные функциональные интерфейсы. [#249209 #240880]

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -6,8 +6,6 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class SearchFolder {
-    Predicate<Folder> pred = p -> p.getSize() > 100 && p.getName().contains("bug");
-
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
         return new ArrayList<>(list).stream()
                 .filter(pred)
