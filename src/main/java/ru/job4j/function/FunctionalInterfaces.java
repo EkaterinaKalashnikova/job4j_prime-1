0680483dc860c9976d6671cffdb859b3f89879bc
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-12-17 12:35:01 +0300

    Функциональный интерфейс [#247497 #240774]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -9,22 +9,20 @@ import java.util.function.*;
 public class FunctionalInterfaces {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        BiConsumer<Integer, String> biCon = (s, s1) -> System.out.println(s1);
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        map.put(4, "four");
-        map.put(5, "five");
-        map.put(6, "six");
-        map.put(7, "seven");
-        int i = 1;
-        for (Map.Entry<Integer, String> s : map.entrySet()) {
-            biCon.accept(i++, " " + s);
-        }
+        BiConsumer<Integer, String> biCon = (i, s) ->  map.put(i, s);
+        biCon.accept(1, "one");
+        biCon.accept(2, "two");
+        biCon.accept(3, "three");
+        biCon.accept(4, "four");
+        biCon.accept(5, "five");
+        biCon.accept(6, "six");
+        biCon.accept(7, "seven");
 
-        BiPredicate<Integer, String> biPred = (j, s) -> j % 2 == 0 || s.length() == 4;
-        for (Integer j : map.keySet()) {
-            System.out.println(biPred.test(j, map.get(j)) + ": " + "key: " + j + " value: " + map.get(j));
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || map.get(i).length() == 4;
+        for (Integer i : map.keySet()) {
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
+            }
         }
 
         Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
@@ -33,8 +31,8 @@ public class FunctionalInterfaces {
         Consumer<String> con = System.out::println;
         Function<String, String> func = String::toUpperCase;
         for (String s : strings) {
-            con.accept(s);
-            System.out.println("Заменили на " + func.apply(s));
+            con.accept(func.apply(s));
+           // System.out.println("Заменили на " + func.apply(s));
         }
     }
 }
