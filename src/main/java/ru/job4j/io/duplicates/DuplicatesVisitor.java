26ae07c787b28af4407be58e315ec4f1065718d4
Ekaterina Kalashnikova <ladykati73@gmail.com>
2022-01-13 21:07:03 +0300

    ListIterator [#350217]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,26 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    Set<FileProperty> unicElement = new HashSet<>();
+    List<FileProperty> array = new ArrayList<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
+        if (unicElement.contains(fileProperty)) {
+            array.add(fileProperty);
+            System.out.println(file.toAbsolutePath());
+        }
+        unicElement.add(fileProperty);
+        return super.visitFile(file, attrs);
+    }
+}
+
+
